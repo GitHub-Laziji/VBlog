7210c6dbbc3701bf09ea46a8072492ef6ca655a6
GitHub-Laziji <c_smtp@163.com>
2018-04-20 00:57:42 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,72 @@
 <template>
     <div>
-        <section style="padding: 5rem 6rem;color: #fff;text-align: center;background-color: #159957;background-image: linear-gradient(120deg, #155799, #159957);">
-            <h1 style="font-size: 3.25rem;margin-top: 0;margin-bottom: 0.1rem;">LaZiji</h1>
-            <h2 style="margin-bottom: 2rem;font-weight: normal;font-size: 1.25rem;opacity: 0.7;">欢迎来到辣子鸡的个人博客.</h2>
+        <section class="page-header">
+            <h1 class="project-name">LaZiji</h1>
+            <h2 class="project-tagline">欢迎来到辣子鸡的个人博客.</h2>
             
-              <a href="https://github.com/GitHub-Laziji" style="text-decoration: none;padding: 0.75rem 1rem;display: inline-block;margin-bottom: 1rem;color: rgba(255,255,255,0.7);background-color: rgba(255,255,255,0.08);border-color: rgba(255,255,255,0.2);border-style: solid;border-width: 1px;border-radius: 0.3rem;transition: color 0.2s, background-color 0.2s, border-color 0.2s;">GitHub</a> 
+            <a href="https://github.com/GitHub-Laziji" class="btn">GitHub主页</a>
+            <a href="https://github.com/GitHub-Laziji/blog-vue" class="btn">博客源码</a> 
           </section>
     </div>
 </template>
+<script>
+    export default{
+        data(){
+            return {
+
+            }
+        }
+    }
+</script>
+
+<style>
+    .page-header{
+        padding: 5rem 6rem;
+        color: #fff;
+        text-align: center;
+        background-color: #159957;
+        background-image: linear-gradient(120deg, #155799, #159957);
+    }
+    .project-name {
+        font-size: 3.25rem;
+        margin-top: 0;
+        margin-bottom: 0.1rem;
+    }
+    .project-tagline {
+        font-size: 1.25rem;
+        margin-bottom: 2rem;
+        font-weight: normal;
+        opacity: 0.7;
+    }
+    .btn:hover {
+        color: rgba(255,255,255,0.8);
+        text-decoration: none;
+        background-color: rgba(255,255,255,0.2);
+        border-color: rgba(255,255,255,0.3);
+    }
+    a:hover {
+        text-decoration: underline;
+    }
+    a:active, a:hover {
+        outline: 0;
+    }
+    .btn {
+        padding: 0.75rem 1rem;
+        display: inline-block;
+        margin-bottom: 1rem;
+        color: rgba(255,255,255,0.7);
+        background-color: rgba(255,255,255,0.08);
+        border-color: rgba(255,255,255,0.2);
+        border-style: solid;
+        border-width: 1px;
+        border-radius: 0.3rem;
+        transition: color 0.2s, background-color 0.2s, border-color 0.2s;
+    }
+    a {
+        color: #1e6bb8;
+        text-decoration: none;
+    }
+    .btn + .btn {
+        margin-left: 1rem;
+    }
+</style>
