958e99b1b4f4cbfe0a8700e03e2d43cafa934a91
辣子鸡 <30425217+GitHub-Laziji@users.noreply.github.com>
2018-04-23 14:49:29 +0800

    submit

--- src/api/gist.js
+++ src/api/gist.js
@@ -3,12 +3,25 @@ import request from '@/utils/request'
 export default {
   test:function(){
     return request({
-      url: '/raw/365370/8c4d2d43d178df44f4c03a7f2ac0ff512853564e/ring.erl'
+      url: ''
     })
   },
   list:function(){
     return request({
       url: 'https://api.github.com/users/GitHub-Laziji/gists'
     })
-  }
+  },
+  create:function(form){
+    let files = {}
+    files[form.title]={content:form.content}
+    return request({
+      url: 'https://api.github.com/gists?access_token=Token',
+      method: 'post',
+      data:{
+        "description": form.description,
+        "public": true,
+        "files": files
+      }
+    })
+  },
 }
--- src/views/blog/Edit.vue
+++ src/views/blog/Edit.vue
@@ -5,8 +5,8 @@
                 <el-form-item label="标题" prop="title">
                     <el-input v-model="form.title"></el-input>
                 </el-form-item>
-                <el-form-item label="简要说明" prop="describe">
-                    <el-input  v-model="form.describe" type="textarea"></el-input>
+                <el-form-item label="简要说明" prop="description">
+                    <el-input  v-model="form.description" type="textarea"></el-input>
                 </el-form-item>
                 <el-form-item label="博客正文" prop="content">
                     <mavon-editor
@@ -14,7 +14,11 @@
                     :subfield="false"/>
                 </el-form-item>
                 <el-form-item>
-                    <el-button type="primary" @click="onSubmit">发表</el-button>
+                    <el-button 
+                    type="primary" 
+                    @click="onSubmit" 
+                    :loading="submitButton.loading"
+                    :disabled="submitButton.disabled">发表</el-button>
                     <el-button @click="$router.push('/user/blog/main')">返回</el-button>
                 </el-form-item>
             </el-form>
@@ -24,12 +28,13 @@
     </div>
 </template>
 <script>
+    import GistApi from '@/api/gist'
     export default{
         data(){
             return {
                 form: {
                     title:"",
-                    describe:"",
+                    description:"",
                     content:""
                 },
                 ruleValidate: {
@@ -37,22 +42,37 @@
                         { required: true, message: '请输入标题', trigger: 'blur' },
                         { type: 'string', max: 32, message: '标题长度不大于32字符', trigger: 'change' }
                     ],
-                    describe: [
+                    description: [
                         { required: true, message: '请输入博客描述', trigger: 'blur' }
                     ],
                     content: [
                         { required: true, message: '请输博客入正文', trigger: 'blur' }
                     ],
+                },
+                submitButton:{
+                    loading:false,
+                    disabled:false
                 }
             }
         },
         methods:{
             onSubmit(){
+                this.submitButton.loading=true
+                this.submitButton.disabled=true
                 this.$refs['form'].validate((valid) => {
                     if (valid) {
-                        this.$Message.success('Success!');
+                        GistApi.create(this.form).then((result)=>{
+                            console.log(JSON.stringify(result))
+                            this.$message({
+                                message: '发表成功',
+                                type: 'success'
+                            })
+                        }).then(()=>{
+                            this.submitButton.loading=false
+                            this.submitButton.disabled=false
+                        })
                     } else {
-                        this.$Message.error('Fail!');
+                        
                     }
                 })
             }
